46e962401465c3d7dd3101ac38463289e0b64af3
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-05-12 10:01:20 +0100

    Add mutate method.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Returns a random DNA base
 const returnRandBase = () => {
-  const dnaBases = ["A", "T", "C", "G"];
+  const dnaBases = ["A", "T", "C", "G"]; // DNA is comprised of four bases (Adenine, Thymine, Cytosine, and Guanine)
   return dnaBases[Math.floor(Math.random() * 4)];
 };
 
@@ -12,3 +12,36 @@ const mockUpStrand = () => {
   }
   return newStrand;
 };
+
+const pAequorFactory = (num, arr) => {
+  return {
+    specimenNum: num,
+    dna: arr,
+
+    mutate() {
+      const dnaBasesToMutate = ["A", "T", "C", "G"];
+      const randomToMutate = returnRandBase();
+      const mutatedGene = dnaBasesToMutate.splice(
+        dnaBasesToMutate.indexOf(randomToMutate),
+        1
+      ); // gives array of gens to mutate -excluding base gen to mutate
+      console.log(dnaBasesToMutate);
+      console.log(mutatedGene);
+
+      //find gen within dna array
+      this.dna.map((gene, index) => {
+        if (gene === mutatedGene[0]) {
+          this.dna[index] =
+            dnaBasesToMutate[
+              Math.floor(Math.random() * dnaBasesToMutate.length)
+            ];
+        }
+      });
+    },
+  };
+};
+
+const one = pAequorFactory(1, mockUpStrand());
+console.log(one.dna);
+console.log(one.mutate());
+console.log(one.dna);
